4bd1ed38fed796fa42a71bdcc1a1e468253ee6ca
nkanthasamy3 <46797844+nkanthasamy3@users.noreply.github.com>
2019-10-26 13:10:56 -0400

    Basic Activity with CAMERA enabled

---
+++ app/src/main/java/com/example/retailgo/LoadingScreen.java
@@ -0,0 +1,32 @@
+package com.example.retailgo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class LoadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading_screen);
+
+        Thread loading = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(5000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        loading.start();
+
+    }
+}
